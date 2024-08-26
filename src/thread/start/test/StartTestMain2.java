e328d52fcccc2410b88b98909fb7ec7eab825422
DESKTOP-F1MCU7O\Mongta <dudrhkd4179@naver.com>
2024-08-26 17:54:57 +0900

    thread start

---
+++ src/thread/start/test/StartTestMain2.java
@@ -0,0 +1,24 @@
+package thread.start;
+
+import static util.MyLogger.log;
+
+public class StartTestMain2 {
+
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    log("value: " + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(runnable, "counter");
+        thread1.start();
+    }
+}
